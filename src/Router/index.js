ee3924c362d4ba2f2f1ed7898eee92cad0572ed8
ganesh4screenmedia <ganeshp@4screenmedia.com>
2021-12-29 22:14:00 +0530

    privacy policy and terms of use changes

--- src/Router/index.js
+++ src/Router/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Route, Routes } from 'react-router';
 import CopyRight from '../Pages/CopyRights';
+import Privacy from '../Pages/PrivacyPolicy';
+import Terms from '../Pages/TermsOfUse';
 
 const Router = function () {
   return (
     <div>
       <Routes>
         <Route path="/" element={<CopyRight />} />
+        <Route path="/Privacy" element={<Privacy />} />
+        <Route path="/Terms" element={<Terms />} />
       </Routes>
     </div>
   );
