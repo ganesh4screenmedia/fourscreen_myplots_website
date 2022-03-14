1254a18dd6f6351ed6fdb177ec3885312affabc5
ganesh4screenmedia <ganeshp@4screenmedia.com>
2022-03-14 17:56:02 +0530

    -Updated terms of use.

--- src/Router/index.js
+++ src/Router/index.js
@@ -4,6 +4,7 @@ import { Route, Routes, Navigate } from 'react-router';
 import ContactUs from '../Pages/ContactUs';
 import CopyRight from '../Pages/CopyRights';
 import Privacy from '../Pages/PrivacyPolicy';
+import Terms from '../Pages/TermsOfUse';
 import AdminLogin from '../Pages/ReportData/AdminLogin';
 import ReportData from '../Pages/ReportData';
 // import Terms from '../Pages/TermsOfUse';
@@ -16,7 +17,7 @@ const Router = function () {
         <Route path="/ReportData" element={<ReportData />} />
         <Route path="/AdminLogin" element={<AdminLogin />} />
         <Route path="/Privacy" element={<Privacy />} />
-        <Route path="/Terms" element={<Privacy />} />
+        <Route path="/Terms" element={<Terms />} />
         <Route path="/Contact" element={<ContactUs />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
